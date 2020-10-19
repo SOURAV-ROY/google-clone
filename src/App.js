2d961892959e2606fb9507eacf2c85b27f0c8cdc
SOURAV ROY <souravroy.info13@gmail.com>
2020-10-19 12:19:22 +0600

    Home Page with  material-ui/icons install

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Home from "./pages/Home";
 
 function App() {
     return (
         <div className="app">
             <h1 style={{border: "4px solid green"}}>Google Clone ⚔</h1>
-            {/*    Home [Full Page]*/}
-            {/*    Search Page*/}
+            {/*    Home [Full Page] */}
+            <Home/>
+            {/*    Search Page [The Result Page] */}
         </div>
     );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+
+    return (
+        <div className='home'>
+            <h1>This is home page</h1>
+            <div className="home_header">
+                <div className="home_headerLeft">
+                    {/*    Link*/}
+                    {/*    Link*/}
+                </div>
+                <div className="home_headerRight">
+                    {/*    Link*/}
+                    {/*    Link*/}
+                    {/*    Icon*/}
+                </div>
+            </div>
+            <div className="home_body">
+
+            </div>
+        </div>
+    );
+
+}
+
+export default Home;
