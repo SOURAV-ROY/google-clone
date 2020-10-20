68bfb8551dd0baba58a1147f0719e7d832f5d59d
SOURAV ROY <souravroy.info13@gmail.com>
2020-10-20 12:01:14 +0600

    useState with useHistory to push search page ✔

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Search.css";
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import {Button} from '@material-ui/core';
+import {useHistory} from "react-router-dom";
 
 function Search() {
+
+    const [input, setInput] = useState("");
+    const history = useHistory();
+
     const search = sou => {
         sou.preventDefault();
+
+        console.log('Click google search button>>>', input);
+
+        history.push("/search");
     }
     return (
-        <div className="search">
+        <form className="search">
             <div className="search_input">
                 <SearchIcon className="search_inputIcon"/>
-                <input/>
+                <input
+                    value={input}
+                    onChange={sou => setInput(sou.target.value)}
+                />
                 <MicIcon/>
             </div>
             <div className="search_buttons">
-                <Button onClick={search} variant="outlined">Google Search</Button>
+                <Button
+                    onClick={search}
+                    type="submit"
+                    variant="outlined"
+                >Google Search</Button>
                 <Button variant="outlined">I'm Feeling Lucky</Button>
             </div>
-        </div>
+        </form>
     );
 }
 
