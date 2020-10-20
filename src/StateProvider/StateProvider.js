02379891ef7aabecd2622512bfde79d2bd4a9a45
SOURAV ROY <souravroy.info13@gmail.com>
2020-10-20 12:57:18 +0600

    Use ContextAPI (StateProvider, reducer) ✔💖

---
+++ src/StateProvider/StateProvider.js
@@ -0,0 +1,15 @@
+import React, {createContext, useContext, useReducer} from 'react';
+
+//Preparing the data layer *******************************************
+export const StateContext = createContext();
+
+export const StateProvider = ({reducer, initialState, children}) =>
+    (
+
+        <StateContext.Provider value={useReducer(reducer, initialState)}>
+            {children}
+        </StateContext.Provider>
+    );
+
+// Hooks which allows us to pull information form the data layer ****************
+export const useStateValue = () => useContext(StateContext);
